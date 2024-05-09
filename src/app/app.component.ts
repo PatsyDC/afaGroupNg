65626108d54a1c88dfa1ea18a580ee3136828920
PatsyDC <patsy.delgado@tecsup.edu.pe>
2024-05-09 08:59:23 -0500

    nav-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '@components/navbar/navbar.component';
+
+const MODULES = [
+  NavbarComponent,
+  RouterOutlet
+]
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MODULES],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/pages/parts-list/parts-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parts-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './parts-list.component.html',
+  styleUrl: './parts-list.component.css'
+})
+export class PartsListComponent {
+
+}
