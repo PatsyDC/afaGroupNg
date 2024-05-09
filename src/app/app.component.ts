a9a36a01340405738ee5ee96509488e5968fdf37
PatsyDC <patsy.delgado@tecsup.edu.pe>
2024-05-09 11:21:27 -0500

    styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '@components/navbar/navbar.component';
 
+
 const MODULES = [
   NavbarComponent,
   RouterOutlet
