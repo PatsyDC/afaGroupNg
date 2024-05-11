2e9258b86c4d4a7e785299188fc328bc9eacc61d
PatsyDC <patsy.delgado@tecsup.edu.pe>
2024-05-11 01:42:42 -0500

    home-page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
-  standalone: true,
-  imports: [],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent {
+export class HomePageComponent{
+
+  carousels = [
+    {
+      img: 'assets/img/1.1.png',
+      title: 'No hay',
+      subtitle: 'Terreno',
+      parrafo: 'Difícil',
+    },
+    {
+      img: 'assets/img/slider.jpeg',
+      title: 'Los mejores animales',
+      subtitle: 'Una mascota que te dará felicidad',
+      parrafo: 'Duo nonumy et dolor tempor no et. Diam sit diam sit diam erat',
+    },
+    {
+      img: 'assets/img/1.1.png',
+      title: 'Los mejores animales',
+      subtitle: 'Una mascota que te dará felicidad',
+      parrafo: 'Duo nonumy et dolor tempor no et. Diam sit diam sit diam erat',
+    },
+  ];
 
 }
+
