c5733f58d305c830c69e5df45c805c6bde6ce9d1
PatsyDC <patsy.delgado@tecsup.edu.pe>
2024-06-01 20:53:50 -0500

    model-producto

--- src/app/pages/parts-list/parts-list.component.ts
+++ src/app/pages/parts-list/parts-list.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { ProductosService } from 'app/core/services/productos.service';
+import { IProducto } from './../../core/models/Producto.model';
+import { Component, NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-parts-list',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './parts-list.component.html',
   styleUrl: './parts-list.component.css'
 })
 export class PartsListComponent {
+  productos?: IProducto[] = [];
+
+  constructor(private productoService: ProductosService) {}
+
+  ngOnInit(): void {
+    this.productoService.allProductos().subscribe((data) => {
+      console.log('data: ',data);
+      this.productos = data;
+    });
+  }
 
 }
